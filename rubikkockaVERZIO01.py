654c083ec7e31de80fdc93e43c57245ba3f461ff
LeviG9901 <gvardian.levente@gmail.com>
2020-05-29 12:02:12 +0200

    Változtattam a kontúrkeresésen, mert a második variáció jobb, illetve beimportálom a kiterített kocka png fájlt és azon is kontúrokat keresve fogom azonosítani a kiterített kocka minden oldalát, és azon belül a kocka minden kisebb kockarészét.

--- rubikkockaVERZIO01.py
+++ rubikkockaVERZIO01.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 zold = cv2.imread("zold.jpg")
-kocka = cv2.imread("kocka.png")
+kitkocka = cv2.imread("kiteritett.png")
 print("Rubik kocka zold oldala importalodik....")
 zold = cv2.resize(zold, (1920,1080))
 print("Kep ujrameretezese: 1920x1080")
@@ -15,8 +15,9 @@ upper_green = np.array([95, 255, 198])
 mask = cv2.inRange(hsv, lower_green, upper_green)
 
 contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-for contour in contours:
+contours2, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+#Régi Kontúr rajzolás (bénábbik)
+"""for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
     area = cv2.contourArea(contour)
     if area>10000:
@@ -24,7 +25,8 @@ for contour in contours:
         x = approx.ravel()[0]
         y = approx.ravel()[1] - 5
         area = cv2.contourArea(contour)
-        print(area)
+        print(area)"""
+green = np.array([0,128,0])
 for cnt in contours:
     if cv2.contourArea(cnt) >10000:
         x,y,w,h = cv2.boundingRect(cnt)
@@ -32,18 +34,36 @@ for cnt in contours:
         #cv2.imshow('Kivagott kontur',zold[y:y+h,x:x+w])
         avg=np.array(cv2.mean(zold[y:y + h, x:x + w])).astype(np.uint8)
         print('Average color (BGR): ',avg)
+        if avg[0] <= 200 and avg[1] >= 150 and avg[2] <= 100:
+            x = 221
+            y = 221
+            for i in range(x):
+                for j in range(y):
+                    kocka[i, j] = green[:3]
         cv2.waitKey(0)
 
-x = 221
-y = 221
-for i in range(x):
-      for j in range(y):
-        kocka[i,j] = avg[:3]
+print(avg[0],avg[1],avg[2])
+#kiterített kocka képen kontúrok keresése
+for cont in contours2:
+    if cv2.contourArea(cnt) >10000:
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(zold,(x,y),(x+w,y+h),(0,255,0),2)
+        #cv2.imshow('Kivagott kontur',zold[y:y+h,x:x+w])
+        avg=np.array(cv2.mean(zold[y:y + h, x:x + w])).astype(np.uint8)
+        print('Average color (BGR): ',avg)
+        if avg[0] <= 200 and avg[1] >= 150 and avg[2] <= 100:
+            x = 221
+            y = 221
+            for i in range(x):
+                for j in range(y):
+                    kocka[i, j] = green[:3]
+        cv2.waitKey(0)
+
 
 
 cv2.imshow("Eredeti kep a zold oldalrol", zold)
 cv2.imshow("Mask", mask)
 cv2.imshow("HSV", hsv)
-cv2.imshow("Kocka", kocka)
+cv2.imshow("Kocka", kitkocka)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
